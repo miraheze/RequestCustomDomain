bd95a32d4a6c48e3849a2fe4b30000cfd7fcf1b1
Vũ Minh Nguyên <vuminhnguyen@songngu.xyz>
2026-03-20 23:35:50 +0700

    Translate special pages aliases to Vietnamese (again) (#108)
    Per CA's request in Discord

--- RequestCustomDomainAliases.php
+++ RequestCustomDomainAliases.php
@@ -6,3 +6,8 @@ $specialPageAliases['en'] = [
 	'RequestCustomDomain' => [ 'RequestCustomDomain' ],
 	'RequestCustomDomainQueue' => [ 'RequestCustomDomainQueue' ],
 ];
+
+$specialPageAliases['vi'] = [
+	'RequestCustomDomain' => [ 'Yêu cầu tên miền tùy chỉnh' ],
+	'RequestCustomDomainQueue' => [ 'Hàng chờ yêu cầu tên miền', 'Hàng chờ yêu cầu tên miền tùy chỉnh' ],
+];
